c381ccb09217ac45e61d15eac979b06fb1ff461a
Regina Nurieva <regginurieva@gmail.com>
2018-08-22 16:28:09 -0700

    add function to hold button

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,50 @@
 var rollDice=0;
+var turnscore=0;
 var total=0;
 $(document).ready(function(){
+
   $("#rollButton").click(function(){
-  rollDice = (Math.floor(Math.random()*6)+1);
-  // console.log(rollDice)
-  // userScore(rollDice);
-  userScore();
+  rollDice();
+  turnScore();
+  });
+
+  $("#holdButton").click(function(){
+  hold();
   });
 });
+function Player(id){
+  this.id=id;
+  this.total=0;
+  // this.turnScore=0;
+}
 
-var userScore = function() {
-  console.log(rollDice);
-  if (rollDice === 1) {
+var instancePlayer = new Player(0);
+var instancePlayer = new Player(1);
+
+console.log(instancePlayer);
+
+var rollDice = function (){
+  result = (Math.floor(Math.random()*6)+1);
+  return result;
+}
+
+var hold = function (){
+  total += turnscore;
+  $("#userTotal").text(total);
+}
+
+Player.prototype.hold = function(){
+  this.total += turnscore
+  turnscore = 0;
+}
+
+var turnScore = function() {
+  // console.log(result);
+  if (result === 1) {
+    turnscore = 0;
     $("#userStatus").text("");
   } else {
-    total += rollDice
-    $("#userStatus").text(total);
+    turnscore += result
+    $("#userStatus").text(turnscore);
 }
 };
